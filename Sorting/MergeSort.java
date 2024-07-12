8b00beb3efb17a116089fd3baf46612f77d9eae3
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-07-12 10:43:56 +0000

    adding code

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,57 @@
+package Sorting;
+
+public class MergeSort {
+    public static void mergeSort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        int mid = si + (ei - si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr,si,mid,ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei){
+        int temp[] = new int[ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k=0;
+
+        while(i <=mid && j<=ei){
+            if(arr[i] <= arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }
+            else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+           
+        }
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+
+        while(j<=ei){
+            temp[k++] = arr[j++];
+        }
+
+        for(i = si, k = 0; i <= ei; i++, k++){
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] =  { 12, 11, 13, 5, 6, 7 };
+
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
+    }
+}
