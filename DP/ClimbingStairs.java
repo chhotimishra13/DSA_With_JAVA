5be8de22ba7238ef35c4036c2d845c6be3bc35f9
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-16 12:11:38 +0000

    Adding DP Questions

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,20 @@
+//using recursion
+
+
+public class ClimbingStairs {
+
+    public static int countWays(int n){
+        if(n == 0){
+            return 1;
+        }
+
+        if(n < 0){
+            return 0;
+        }
+        return countWays(n - 1) + countWays(n - 2);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(countWays(n));
+    }
+}
