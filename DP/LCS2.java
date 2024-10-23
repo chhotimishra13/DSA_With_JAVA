af00b909da643eeb032e0a8a39b1dccdcfd6eda5
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-23 09:30:14 +0000

    Adding DP Questions

--- DP/LCS2.java
+++ DP/LCS2.java
@@ -1,9 +1,50 @@
-//using tabulation - 0(n)
+//using tabulation - 0(n*m)
 
 public class LCS2 {
+
+    public static int lcsTab(String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n+1][m+1];
+
+        //initialization
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<m+1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] =  Math.max(ans1, ans2);
+                }
+            }
+        }
+       print(dp);
+        return dp[n][m];
+    }
+
+    public static void print(int dp[][]){
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                System.out.print(dp[i][j] +" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         String str1 = "abcde";
         String str2 = "ace";  //ans = 3; //ace
-        System.out.println(longestCommonSubsequence(str1, str2, str1.length(), str2.length()));
+        System.out.println(lcsTab(str1, str2));
     }
 }
