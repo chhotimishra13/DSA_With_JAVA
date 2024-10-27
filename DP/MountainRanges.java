60ea9869a145a9e7f12caf766706c6b9962b8df0
Chhoti Mishra <137808761+chhotimishra13@users.noreply.github.com>
2024-10-27 11:04:26 +0000

    Mountain range in dp

---
+++ DP/MountainRanges.java
@@ -0,0 +1,23 @@
+//TC- O(n^2)
+
+public class MountainRanges{
+    public static int mountainRanges(int n){
+        int dp[] = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<n+1; i++){
+            for(int j=0; j<i; j++){
+                int inside = dp[j];
+                int outside = dp[i-j-1];
+                dp[i] += inside * outside;
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(mountainRanges(n));
+    }
+}
